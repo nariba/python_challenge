992230f6b52ca685ffe95a4deeb5d7988124384d
nariba <2IE15092R@s.kyushu-u.ac.jp>
2016-08-12 18:27:11 +0900

    改行が抜けてたので追加しました

--- problem1.py
+++ problem1.py
@@ -2,7 +2,7 @@
 
 import sys
 
-text = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+text = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.\n"
 
 if __name__ ==  "__main__":
     for i in text:
